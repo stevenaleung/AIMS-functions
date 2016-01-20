72c5614686cc6aa576c6bebcba30f6b690dc1222
Steve Leung <stevealeung@gmail.com>
2016-01-19 18:58:38 -0800

    updated readAIMS to handle multiple parameters

--- functions/readAIMS.m
+++ functions/readAIMS.m
@@ -26,7 +26,7 @@ function data = readAIMS(fileName, plots, folderName, newcAxis, newAxis)
 %
 % -- edit history --
 % Patrick Ye, Butts Pauly Lab, Stanford University
-% SAL   2015-11-23
+% 2015-11-23 SAL    
 
 
 % if plots should be displayed
@@ -55,103 +55,116 @@ end
 %  ^
 %  axis 2
 
+% AIMSfile = loadTextFile(fileName);
+% inds = findLines(AIMSfile, '2D Scan Data');
+% numParams = numel(inds);
 
 % calculate number of header lines before data starts
 text = fileread(fileName);
-dataHeader = '2D Scan Data 0';
+dataHeader = '2D Scan Data';
 newline = '[\n]';
 n = regexp(text, newline);
 m = regexp(text, dataHeader);
-numHeaderLines = sum(n<m);
+numParams = numel(m);
+numHeaderLines = zeros(size(m));
+for i = 1:numParams
+    numHeaderLines(i) = sum(n<m(i));
+end
 
-% read file
-fid = fopen(fileName);
-c = textscan(fid, '%f', 'Headerlines', numHeaderLines+1); % may not be 252 lines for every file...
-data = c{1};
-fclose(fid);
+rawData = cell(numParams, 1);
+paramName = cell(numParams, 1);
+for i = 1:numParams
+    % read file
+    fid = fopen(fileName);
+    c = textscan(fid, '%f', 'Headerlines', numHeaderLines(i)+1); % may not be 252 lines for every file...
+    data = c{1};
+    fclose(fid);
 
-% find out what parameter it is
-paramHeader = 'Parameter 0';
-param = regexp(text, paramHeader);
-ind = find(param < n, 1, 'first');
-paramName = text(param+length(paramHeader)+1:n(ind)-2);
+    % find out what parameter it is
+    paramHeader = sprintf('Parameter %d', i-1);
+    param = regexp(text, paramHeader);
+    ind = find(param < n, 1, 'first');
+    paramName{i} = text(param+length(paramHeader)+1:n(ind)-2);
 
-% calculate x axis properties
-x = regexp(text, '[\n]First Axis');
-xAxisNum = str2double(text(x+12));
-xName = axisAIMS(str2double(text(x+12)));
-dx = data(2) - data(1);
-xMin = data(1);
+    % calculate x axis properties
+    x = regexp(text, '[\n]First Axis');
+    xAxisNum = str2double(text(x+12));
+    xName = axisAIMS(str2double(text(x+12)));
+    dx = data(2) - data(1);
+    xMin = data(1);
 
-xMaxIndex = n(numHeaderLines+2)-2;
-a = text(xMaxIndex);
-while isspace(a) ~= 1
-    xMaxIndex = xMaxIndex - 1;
+    xMaxIndex = n(numHeaderLines+2)-2;
     a = text(xMaxIndex);
-end
-xMax = str2double(text(xMaxIndex:n(numHeaderLines+2)-2));
-
-numXPoints = (xMax - xMin) / dx + 1;
-numXPoints = int32(numXPoints);
-xAxis = data(1:numXPoints);
-
-% 1 mm offset for AIMS software because it doesn't run if Left/Right axis
-% starts at 0 mm
-if xMin == 1 && xAxisNum == 0
-    xAxis = xAxis - 1;
-end
-
-% move first row into an axis variable
-data = data(numXPoints+1:end);
-
-% reshape matrix
-bigData = reshape(data, numXPoints+1, []);
-bigData = bigData';
-rawData = bigData(:, 2:end);
-
-% y axis properties
-yAxis = bigData(:, 1);
-y = regexp(text, '[\n]Second Axis');
-yName = axisAIMS(str2double(text(y+13)));
-
-
-% -------
-% VOLTAGE plot (note: up down is switched compared to AIMS plots)
-if plots
-    figure
-    imagesc([xAxis(1) xAxis(end)], [yAxis(1) yAxis(end)], rawData) % Vpp
-    colormap(hot)
-    colorbar
-    cAxis1 = caxis;
-    cAxis1(1) = 0; % set cAxis min to 0
-    caxis(cAxis1);
-    title(fileName, 'interpreter', 'none')
-    xlabel(xName)
-    ylabel(yName)
-    h = colorbar;
-    xlabel(h, 'Vpp')
-    if nargin == 4 % if new axis are requested
-        axis(newAxis)
+    while isspace(a) ~= 1
+        xMaxIndex = xMaxIndex - 1;
+        a = text(xMaxIndex);
     end
-    axis equal
-    axis([xAxis(1) xAxis(end) yAxis(1) yAxis(end)])
+    xMax = str2double(text(xMaxIndex:n(numHeaderLines+2)-2));
 
-% new cAxis specified
-if nargin >= 3
-    if isnumeric(newcAxis) % if numeric new cAxis specified and nonzero
-        if numel(newcAxis) == 2 % skip if newcAxis == []
-            caxis(newcAxis);
+    numXPoints = (xMax - xMin) / dx + 1;
+    numXPoints = int32(numXPoints);
+    xAxis = data(1:numXPoints);
+
+    % 1 mm offset for AIMS software because it doesn't run if Left/Right axis
+    % starts at 0 mm
+    if xMin == 1 && xAxisNum == 0
+        xAxis = xAxis - 1;
+    end
+
+    % move first row into an axis variable
+    data = data(numXPoints+1:end);
+
+    % reshape matrix
+    bigData = reshape(data, numXPoints+1, []);
+    bigData = bigData';
+    rawData{i} = bigData(:, 2:end);
+    
+    % y axis properties
+    yAxis = bigData(:, 1);
+    y = regexp(text, '[\n]Second Axis');
+    yName = axisAIMS(str2double(text(y+13)));
+
+    % -------
+    % VOLTAGE plot (note: up down is switched compared to AIMS plots)
+    if plots
+        figure;
+        if strcmp(paramName{i}, 'Negative Peak Voltage')
+            % invert the values for peak negative voltage so that the plots have
+            % similar color axes
+            imagesc([xAxis(1) xAxis(end)], [yAxis(1) yAxis(end)], -rawData{i}) % Vpp
+        else
+            imagesc([xAxis(1) xAxis(end)], [yAxis(1) yAxis(end)], rawData{i}) % Vpp
         end
-    elseif ischar(newcAxis) % if new cAxis is the cAxis from another scan
-        load(newcAxis)
-        eval(['cAxis = ' newcAxis '.cAxis;'])
-        caxis(cAxis);
-    end
-end
+        colormap(hot)
+        colorbar
+        title(sprintf('%s, %s', fileName, paramName{i}));
+        xlabel(xName)
+        ylabel(yName)
+        h = colorbar;
+        xlabel(h, 'Vpp')
+        if nargin == 4 % if new axis are requested
+            axis(newAxis)
+        end
+        axis equal
+        axis([xAxis(1) xAxis(end) yAxis(1) yAxis(end)])
 
-cAxis = caxis;
-cAxis(1) = 0; % set cAxis min to 0
-caxis(cAxis);
+    % new cAxis specified
+    if nargin >= 3
+        if isnumeric(newcAxis) % if numeric new cAxis specified and nonzero
+            if numel(newcAxis) == 2 % skip if newcAxis == []
+                caxis(newcAxis);
+            end
+        elseif ischar(newcAxis) % if new cAxis is the cAxis from another scan
+            load(newcAxis)
+            eval(['cAxis = ' newcAxis '.cAxis;'])
+            caxis(cAxis);
+        end
+    end
+
+    cAxis = caxis;
+    cAxis(1) = 0; % set cAxis min to 0
+    caxis(cAxis);
+    end
 end
 
 clear data
